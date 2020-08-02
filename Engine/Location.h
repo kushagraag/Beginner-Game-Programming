972b58fbb694068d645de10e9c230a704bb8a4a5
Kushagra Agrawal <57141343+kushagraag@users.noreply.github.com>
2020-08-02 15:42:43 +0530

    add operator==

--- Engine/Location.h
+++ Engine/Location.h
@@ -1,12 +1,16 @@
 #pragma once
 
-struct Location
+class Location
 {
 public:
 	void Add(const Location& loc)
 	{
 		x += loc.x;
 		y += loc.y;
+	}
+	bool operator==(const Location& rhs) const
+	{
+		return x == rhs.x && y == rhs.y;
 	}
 	int x;
 	int y;
