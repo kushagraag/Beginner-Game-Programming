69562e29b14ff7d082eb1fe90ab120010fc2f3b3
Kushagra Agrawal <57141343+kushagraag@users.noreply.github.com>
2020-08-02 11:06:41 +0530

    Added Board

---
+++ Engine/Board.cpp
@@ -0,0 +1,26 @@
+#include "Board.h"
+#include <assert.h>
+Board::Board(Graphics& gfx)
+	:
+	gfx( gfx )
+{
+}
+
+void Board::DrawCell(Location& loc, Color c)
+{
+	assert(loc.x >= 0);
+	assert(loc.x < width);
+	assert(loc.y >= 0);
+	assert(loc.y >= height);
+	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension, dimension, c);
+}
+
+int Board::GetBoardWidth()
+{
+	return width;
+}
+
+int Board::GetBoardHeight()
+{
+	return height;
+}
