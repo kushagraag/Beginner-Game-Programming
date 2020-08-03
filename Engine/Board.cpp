95302fa65b978e2f17437fb5ab1fe7e4918e6db4
Kushagra Agrawal <57141343+kushagraag@users.noreply.github.com>
2020-08-03 15:28:37 +0530

    Added Border in Board

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -12,7 +12,10 @@ void Board::DrawCell(const Location& loc, Color c)
 	assert(loc.x < width);
 	assert(loc.y >= 0);
 	assert(loc.y < height);
-	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension, dimension, c);
+	const int off_x = x + borderWidth + borderPadding;
+	const int off_y = y + borderWidth + borderPadding;
+	gfx.DrawRectDim(loc.x * dimension + off_x + cellPadding, loc.y * dimension + off_y + cellPadding, 
+		dimension - cellPadding * 2, dimension - cellPadding * 2, c);
 }
 
 int Board::GetBoardWidth()
@@ -29,4 +32,21 @@ bool Board::IsInsideBoard(const Location loc) const
 {
 	return loc.x >= 0 && loc.x < width&&
 		loc.y >= 0 && loc.y < height;
+}
+
+void Board::DrawBorder()
+{
+	const int top = y;
+	const int left = x;
+	const int bottom = top + (borderWidth + borderPadding) * 2 + height * dimension;
+	const int right = left + (borderWidth + borderPadding) * 2 + width * dimension;
+
+	//top
+	gfx.DrawRect(left, top, right, top + borderWidth, borderColor);
+	//bottom
+	gfx.DrawRect(left, bottom - borderWidth, right, bottom, borderColor);
+	//left
+	gfx.DrawRect(left, top + borderWidth, left + borderWidth, bottom - borderWidth, borderColor);
+	//right
+	gfx.DrawRect(right - borderWidth, top + borderWidth, right, bottom - borderWidth, borderColor);
 }
--- Engine/Board.h
+++ Engine/Board.h
@@ -12,9 +12,17 @@ public:
 	int GetBoardWidth();
 	int GetBoardHeight();
 	bool IsInsideBoard(const Location loc) const;
+	void DrawBorder();
 private:
+	static constexpr Color borderColor = Colors::Blue;
 	static constexpr int dimension = 20;
-	static constexpr int width = 25;
+	static constexpr int width = 35;
 	static constexpr int height = 25;
+	static constexpr int cellPadding = 1;
+	static constexpr int borderWidth = 4;
+	static constexpr int borderPadding = 2;
+	static constexpr int x = 45;
+	static constexpr int y = 45;
+
 	Graphics& gfx;
 };
